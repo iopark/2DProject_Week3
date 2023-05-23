7844af2bbb279989c10e0511b64f10fb423f6a53
here_io <52244687+iopark@users.noreply.github.com>
2023-05-23 21:48:32 +0900

    feat: HW 0523

---
+++ Assets/Scripts/Enemy/Mouse.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [Header("Pertaining to Coposition")]
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    [Header("Movement")]
+    private float moveSpeed;
+    private int moveDir; 
+
+    [Header("Sight")]
+    [SerializeField] private Transform sightBox;
+    [SerializeField] private LayerMask groundMask; 
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void Start()
+    {
+        moveDir = -1; 
+        moveSpeed = 3; 
+    }
+    private void Update()
+    {
+        //Do default activity 
+        Move();
+        if (!IsGroundExist())
+        {
+            Turn(); 
+        }
+        if (WallCheck())
+        {
+            Turn(); 
+        }
+    }
+
+    private void Move()
+    {
+        // certain behavoir, defined by the state 
+        rb.velocity = new Vector2(moveSpeed *moveDir, rb.velocity.y); 
+    }
+
+    private void Turn()
+    {
+        transform.Rotate(Vector3.up, 180); // Alternative to the spriterenderer flipX.
+        moveDir *= -1; // flip marching direction 
+        //if (!spriteRenderer.flipX)
+        //{
+        //    spriteRenderer.flipX = true;
+        //    return;
+        //}
+        //else
+        //{
+        //    spriteRenderer.flipX = false;
+        //    return;
+        //}
+    }
+
+    private bool IsGroundExist()
+    {
+        Debug.DrawRay(sightBox.position, Vector2.down, Color.yellow);
+        return Physics2D.Raycast(sightBox.position, Vector2.down, 1.5f, groundMask); // would return true everytime raycast projectile contacts with the ground layer. 
+    }
+    private bool WallCheck() // 우리 쥐는 벽도 가릴줄 알아요 
+    {
+        Vector2 mouseFace = new Vector2 (transform.position.x, transform.position.y -0.3f); //given 마우스는 왼쪽을 바라보며 시작하니) 
+        Debug.DrawRay(mouseFace, rb.velocity.normalized, Color.green, 0);
+        return Physics2D.Raycast(transform.position, rb.velocity.normalized, 0.8f, groundMask);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,23 +15,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator anim;
     [SerializeField] private SpriteRenderer spriteRenderer;
 
+    [Header("JumpDetect")]
+    [SerializeField] private bool isGrounded;
+    [SerializeField] LayerMask groundLayer; // you can select multiple, single, or non amount of layers in the inspector view. 
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
-
     private void Start()
     {
         jumpPower = 10;
         movePower = 10;
         maxSpeed = 5; 
     }
-
     private void Update()
     {
         Move(); 
+    }
+    /// <summary>
+    /// 가능한 물리적인 연산들(Raycast, Physics)들은 고정update 로 구현하는것이 ㅈ2ㅓㅇ배다. 
+    /// </summary>
+    private void FixedUpdate()
+    {
+        GroundCheck(); 
     }
     private void Move()
     {
@@ -54,7 +63,7 @@ public class PlayerController : MonoBehaviour
     private void Jump()
     {
         rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        anim.SetBool("onLand", false);
+        anim.SetBool("hasLanded", false);
     }
     private void OnMove(InputValue value)   
     {
@@ -69,19 +78,41 @@ public class PlayerController : MonoBehaviour
     }
     private void OnJump(InputValue value)
     {
-        anim.SetBool("onLand", false);
+        //anim.SetBool("hasLanded", false);
         Jump(); 
     }
-    /// <summary>
-    /// 땅에 접촉유무를 위해 충돌체 를 이용한다. 
-    /// </summary>
-    /// <param name="collision"></param>
-    private void OnCollisionEnter2D(Collision2D collision)
+    //Ground Interaction: 2가지 방법이 존재합니다. 
+    //1. RayCast 를 응용하여 땅 찾는방법 Landing Interaction 
+    //2. Collider객체를 하위자식으로 두어 collider 에 따른 eventFunction 으로 반응하는 방법 
+    private void GroundCheck()
     {
-        anim.SetBool("onLand", true); 
+
+        RaycastHit2D groundDetect = Physics2D.Raycast(transform.position, Vector2.down, 1.05f, groundLayer); 
+        Debug.DrawRay(transform.position, Vector2.down, Color.red, 0);
+
+        if (groundDetect.collider != null)
+        {
+            isGrounded = true;
+            anim.SetBool("hasLanded", true);
+            Debug.DrawRay(transform.position, new Vector3(groundDetect.point.x, groundDetect.point.y, 0) - transform.position, Color.red, 0);
+        }
+        else
+        {
+            isGrounded= false;
+            anim.SetBool("hasLanded", false);
+            Debug.DrawRay(transform.position, Vector2.down, Color.red, 0);
+        }
     }
-    private void OnCollisionExit2D(Collision2D collision)
+
+    private bool WallCheck()
     {
-        anim.SetBool("onLand", false); 
+        //Vector2 dirFront = new Vector2 (transform.position.x * -0.4f, transform.position.y); //given 마우스는 왼쪽을 바라보며 시작하니) 
+        Debug.DrawRay(transform.position, Vector2.left, Color.green, 0); 
+        return Physics2D.Raycast(transform.position, Vector2.left, 0.8f, groundLayer);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+            anim.SetBool("hasLanded", true); 
     }
 }
